40d96ec9d3f3b76d3ebe4c119ba0325ddb03e0ce
ibnulwahid <77407897+ibnulwahid@users.noreply.github.com>
2021-01-26 12:07:37 +0600

    Add files via upload

---
+++ Untitled Folder/precache-manifest.1a6d4698b826b1b17f5a50e3326d4e6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5dd0241ad1521e975aaf67bdb859692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06f7be56a48f0a05111",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "a1dc9f706baf6ba42a72",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "d06f7be56a48f0a05111",
+    "url": "/static/js/2.644cc6b3.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.644cc6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1dc9f706baf6ba42a72",
+    "url": "/static/js/main.e2107b15.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
